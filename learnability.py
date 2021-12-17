dd83ec9df1611c66cc2f71fc73c0fd2f4a13cbce
Balint Gyevnar <gyevnarb@gmail.com>
2021-12-17 02:33:15 +0000

    Learnability now supports writing and reading to file in WCS format

--- learnability.py
+++ learnability.py
@@ -1,8 +1,6 @@
 import pickle
 import os
-import argparse
-from dataclasses import dataclass
-from typing import Tuple, List, Optional
+from typing import List, Dict, Tuple
 
 import pandas as pd
 import numpy as np
@@ -10,248 +8,241 @@ import matplotlib.pyplot as plt
 
 from scipy.stats import expon, beta, multivariate_normal
 from ck_blahut_arimoto import ck_blahut_arimoto_ib
+from noga.figures import grid2img
+from noga.tools import DKL, MI
 from tqdm import tqdm
 
 
-def fit_gaussians(data: pd.DataFrame, cov_reg: float = 1e-5) \
-        -> Tuple[np.ndarray, List[Tuple[str, np.ndarray, np.ndarray]]]:
-    """ Fit Gaussians to the given data conditioned on colour terms.
+def plot_color_prior(pc: np.array) -> None:
+    """ Plot the color matrix in the WCS format.
 
-    Args:
-        data: Data points of colour term and colour stimulus observations
-        cov_reg: The covariance regularisation constant
-
-    Returns:
-        Tuple of the conditional distribution p(C|W) and the gaussian model params for each w in W
-    """
-    # By fitting conditional Gaussians compute the distribution p(C|W)
-    pc_w = []
-    model_params = []
-    for ct, subset in data.groupby("word"):
-        subset = subset[["L*", "a*", "b*"]]
-
-        mu, cov = subset.mean().to_numpy(), subset.cov().to_numpy()
-        if subset.shape[0] == 1:
-            cov = np.identity(3)
-        cov += np.identity(3) * cov_reg
-        proba = multivariate_normal(mu, cov).pdf(chip2lab)
-
-        model_params.append((ct, mu, cov))
-        pc_w.append(proba / sum(proba))
-    return np.array(pc_w), model_params
+     Args:
+         pc: The prior distribution over colors.
+     """
+    grid = np.repeat(pc[:, None], 3, axis=1) / pc.max()
+    img = np.flipud(grid2img(grid))
+    r = img[:, :, 0]
+    g = img[:, :, 1]
+    b = img[:, :, 2]
+    clrs = np.array([r.flatten(), g.flatten(), b.flatten()]).T
+    ax = plt.pcolor(r, color=clrs, linewidth=0.04, edgecolors="None")
+    ax.set_array(None)
+    plt.xlim([0, 42])
+    plt.ylim([0, 10])
+    plt.xticks([])
+    plt.yticks([])
+    plt.axis("off")
+    plt.show()
 
 
-def learn_language(data: pd.DataFrame) -> np.ndarray:
-    """ Learn the joint distribution of colour stimuli and terms given data.
+class GaussianLanguageModel:
+    """ Load and fit Gaussian models to each language from data stored in the WCS format. """
+    # Excluded language IDs for color prior calculation, as given by Noga.
+    COLOR_PRIOR_EXCLUDED = [7, 19, 20, 25, 27, 31, 38, 48, 70, 80, 88, 91, 92, 93]
 
-    Args:
-        data: Data containing all observations of colour terms and stimuli for the language
+    def __init__(self, term_file: str = "wcs/term.txt", wcs_path: str = "wcs", cov_reg: float = 1.e-5):
+        """ Initialise a new model class. Language models are stored in the dictionary self.models which is indexed by
+        the ID of the language.
 
-    Returns:
-        The distribution p(W, C)
-    """
-    # Compute p(C|W)
-    pc_w, models = fit_gaussians(data)
+        Args:
+            term_file: The file containing the color term elicitations
+            wcs_path: Path to the directory containing the WCS data
+            cov_reg: Covariance regularisation hyper-parameter
+        """
+        self.wcs_path = wcs_path
+        self.cov_reg = cov_reg
+        self.models = {}
+        self.models_params = {}
 
-    # Compute p(W, C|H)
-    pw_h = 1  # This assumes that meanings to colour terms are one-to-one
-    pwc_h = pc_w * pw_h
-    pwc_h /= pwc_h.sum()
+        self.term_data = pd.read_csv(term_file, sep="\t", names=["language", "speaker", "chip", "word"])
+        self.chip_data = pd.read_csv(os.path.join(wcs_path, "chip.txt"), sep="\t", index_col=0,
+                                     names=["row", "col", "pos"])
+        self.chip_to_lab = pd.read_csv(os.path.join(wcs_path, "cnum-vhcm-lab-new.txt"), sep="\t", header=0,
+                                       index_col="#cnum")
 
-    return pwc_h, models
+        self.sample_size = dict(self.term_data.groupby("language").size())
+        self.chip_to_lab = self.chip_to_lab.sort_values(by="#cnum")[["L*", "a*", "b*"]]
+        self.term_data = self.term_data.merge(self.chip_to_lab, left_on="chip", right_on="#cnum", how="left")
 
+    def calculate_color_prior(self) -> np.array:
+        """ Calculate capacity-inducing prior over the colour chip space."""
+        assert self.models != {}, "No languages learnt. Try calling learn_lanuages()."
 
-def plot_gaussians(data: pd.DataFrame, models: List[Tuple[str, np.ndarray, np.ndarray]],
-                   vars: Tuple[str, str] = (0, 2), axis: plt.Axes = None):
-    """ Contour-plot of Gaussian distributions for each colour term and the corresponding data points.
+        L = 0
+        pc = np.zeros(self.chip_data.shape[0])
 
-    Args:
-        data: Same dataframe that was used to fit each model in models
-        models: Mean and covariance matrices for each Gaussian model fitted
-        vars: Indices of which two variables from L*A*B* to select for plotting
-        axis: Optional axis to draw on
-    """
-    vars = list(vars)
-    vars_name = np.array(["L*", "a*", "b*"])[vars]
+        for lang_id, pwc in self.models.items():
+            if lang_id in self.COLOR_PRIOR_EXCLUDED:
+                continue
 
-    if axis is None:
-        fig, axis = plt.subplots()
+            _, q_wct = ck_blahut_arimoto_ib(pwc.to_numpy(), 1, np.full(pwc.shape, 1 / pwc.size), "kl-divergence")
+            plc = q_wct.sum(axis=0).sum(axis=1)
+            pc += plc
+            L += 1
 
-    for i, (ct, subset) in enumerate(data.groupby("word")):
-        subset = subset[vars_name]
-        x_data, y_data = subset[vars_name[0]], subset[vars_name[1]]
-        x_min, x_max = x_data.min() - 1, x_data.max() + 1
-        y_min, y_max = y_data.min() - 1, y_data.max() + 1
+        pc /= L
+        return pc
 
-        _, mu, cov = models[i]
+    def learn_languages(self, language_ids: List[int] = None, progress_bar: bool = True) -> None:
+        """ Fit a Gaussian model to each language to calculate the joint distribution P(W, C). Fitted models are stored
+        in the field self.models
 
-        mu = mu[vars]
-        cov = cov[vars, :]
-        cov = cov[:, vars]
+        Args:
+            language_ids: Only fit to the languages with the given IDs
+            progress_bar: Show progress bar
+        """
+        if progress_bar:
+            progress = tqdm(self.term_data.groupby('language'))
+        else:
+            progress = self.term_data.groupby('language')
 
-        n = 100
-        x = np.linspace(x_min, x_max, n)
-        y = np.linspace(y_min, y_max, n)
-        x, y = np.meshgrid(x, y)
+        for i, (lang_id, data) in enumerate(progress):
+            if language_ids is not None and lang_id not in language_ids:
+                continue
 
-        # Pack X and Y into a single 3-dimensional array
-        pos = np.empty(x.shape + (2,))
-        pos[:, :, 0] = x
-        pos[:, :, 1] = y
+            nd = data.shape[0]  # Number of datapoints for this language
+            nw = data["word"].nunique()  # Number of unique colour terms
 
-        z = multivariate_normal(mu, cov).pdf(pos)
-        axis.contour(x, y, z)
-        axis.scatter(x_data.values, y_data.values, marker="o")
-    axis.set_title("Fitted Gaussian contours")
-    axis.set_xlabel(vars_name[0])
-    axis.set_ylabel(vars_name[1])
-    return axis
+            if progress_bar:
+                progress.set_postfix({"lang": lang_id, "nd": nd, "nw": nw})
 
+            # Fit Gaussian models to the color chips to get P(C|W)
+            pc_w = []
+            model_params = {}
+            for ct, subset in data.groupby("word"):
+                subset = subset[["L*", "a*", "b*"]]
 
-@dataclass
-class Model:
-    lang_id: int
-    lang_str: str
-    n_obs: int
-    n_words: int
-    pwc_h: np.ndarray
-    plc: Optional[np.ndarray]
+                mu, cov = subset.mean().to_numpy(), subset.cov().to_numpy()
+                if subset.shape[0] == 1:
+                    cov = np.identity(3)
+                cov += np.identity(3) * self.cov_reg
+                proba = multivariate_normal(mu, cov).pdf(self.chip_to_lab)
+
+                model_params[ct] = (mu, cov)
+                pc_w.append(proba / sum(proba))
+            pc_w = np.array(pc_w)
+            pwc = pc_w / pc_w.sum()
+
+            self.models[lang_id] = pd.DataFrame(pwc, index=model_params.keys())
+            self.models_params[lang_id] = model_params
+
+    def sample_languages(self, n: int = 5) -> pd.DataFrame:
+        """ Sample languages from the learned models.
+
+        Args:
+            n: The number of samples to draw from each languages' joint distribution.
+
+        Returns: A pandas dataframe in the format of the wcs/terms.txt file.
+        """
+        samples = []
+
+        for lang_id, pwc in self.models.items():
+            sample_idx = np.random.choice(np.arange(pwc.size), (n,), True, pwc.to_numpy().flatten())
+            words_list = list(self.models_params[lang_id].keys())
+            for idx in sample_idx:
+                word_idx, chip_idx = idx // pwc.shape[1], idx % pwc.shape[1]
+
+                speaker = 1
+                color = chip_idx + 1
+                word = words_list[word_idx]
+
+                samples.append((lang_id, speaker, color, word))
+
+        return pd.DataFrame(samples, columns=["language", "speaker", "chip", "word"])
+
+    @staticmethod
+    def write_samples_file(samples: pd.DataFrame, output_file: str = "sampled_term.txt") -> None:
+        """ Write a language sample to file in the wcs/term.txt format. """
+        samples.to_csv(output_file, sep="\t", index=False, header=False)
+
+    @staticmethod
+    def score_languages(adult: "GaussianLanguageModel", child: "GaussianLanguageModel", pc: np.array = None) \
+            -> Dict[int, Tuple[float, float]]:
+        """ For each real-world language score a hypothetical child language learnt on restricted number of sampled data
+        against an adult language learnt on complete data.
+
+        Args:
+             adult: The adult model learnt on complete data
+             child: The child model learnt on sampled, partial data
+             pc: The color prior
+
+        Returns:
+            A dictionary of tuples in the form (mutual information, log-likelihood) for each language id.
+        """
+        if pc is None:
+            pc = np.full((330, ), 1 / 330)
+
+        scores = {}
+        # samples = child.term_data.groupby("language")
+        for lang_id in child.models:
+            pwc_h = child.models[lang_id].to_numpy()
+            pwc = adult.models[lang_id].to_numpy()
+
+            # Simplicity prior
+            n = child.sample_size[lang_id]
+            nw = len(child.models_params[lang_id])
+            ph = 1 # expon.pdf(nw, scale=10)
+
+            # word2idx = {w: i for (i, w) in enumerate(child.models_params[lang_id])}
+            # sample = samples.get_group(lang_id)
+            # sample = np.vstack([
+            #     sample["word"].apply(lambda x: word2idx[x]),
+            #     sample["chip"] - 1
+            # ]).T
+            # ll = np.log(np.take(pwc_h, sample)) + np.log(ph)
+            # ll[ll == -np.inf] = 0.0
+            # inf_loss = ll.sum()
+
+            pwc_h_ = np.zeros_like(pwc)
+            shared_idx = [i for i, w in enumerate(adult.models_params[lang_id])
+                           if w in child.models_params[lang_id]]
+            pwc_h_[shared_idx, :] = pwc_h
+            inf_loss = DKL(pwc, pwc_h_)
+
+            # Compute complexity (mutual information)
+            pc_h = pwc_h.sum(axis=0)
+            lratio = np.log(pc_h) - np.log(pwc.sum(axis=0))
+            lratio[lratio == -np.inf] = 0
+            mutual_info = np.sum(pc_h * ph * lratio)
+
+            scores[lang_id] = np.array([mutual_info, inf_loss])
+        return scores
 
 
 if __name__ == '__main__':
     np.seterr(divide='ignore')
     np.random.seed(42)
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--n_min", type=int, help="The minimum number of samples to draw for each language.",
-                        default=1)
-    parser.add_argument("--n_step", type=int, help="The step rate of the number of samples to draw.",
-                        default=1)
-    parser.add_argument("--n_max", type=int, help="The maximum number of samples to draw for each language.",
-                        default=100)
-    parser.add_argument("-w", "--wcs_path", type=str, help="Path to the directory containing World Color Survey data.",
-                        default="wcs")
-    parser.add_argument("-l", "--language", type=str, help="Specify name of language to only generate samples for.")
-    parser.add_argument("-o", "--output", type=str, help="Output path", default="")
-    args = parser.parse_args()
+    adult_model = GaussianLanguageModel()
+    adult_model.learn_languages()
 
-    print("Running learnability problem")
-
-    print(f"Loading WCS survey data from \"{args.wcs_path}\" - ", end="")
-    wcs_path = args.wcs_path
-    color_data_integer = pd.read_csv(os.path.join(wcs_path, "term.txt"),
-                                     sep="\t", names=["language", "speaker", "chip", "word"])
-    lang_id_lang = pd.read_csv(os.path.join(wcs_path, "lang.txt"),
-                               sep="\t", usecols=[0, 1], index_col=0, names=["id", "language"])
-    chip_data = pd.read_csv(os.path.join(wcs_path, "chip.txt"),
-                            sep="\t", index_col=0, names=["row", "col", "pos"])
-    chip2lab = pd.read_csv(os.path.join(wcs_path, "cnum-vhcm-lab-new.txt"),
-                           sep="\t", header=0, index_col="#cnum")
-
-    chip2lab = chip2lab.sort_values(by="#cnum")[["L*", "a*", "b*"]]
-    color_data_integer = color_data_integer.merge(chip2lab, left_on="chip", right_on="#cnum", how="left")
-    covariance_prior = color_data_integer[["L*", "a*", "b*"]].cov().to_numpy()
-    print("done")
-
-    C = chip2lab.shape[0]  # Number of colour chips
-    N_min = args.n_min  # Number of samples to draw
-    N_step = args.n_step  # Number of samples to draw
-    N_max = args.n_max  # Number of samples to draw
-
-    print("Trying to load colour prior p(C) - ", end="")
-    pC = None
-    if os.path.exists("pC.p"):
-        pC = pickle.load(open("pC.p", "rb"))
-        print("done")
+    # Calculate color prior once
+    if not os.path.exists("pc.p"):
+        pc = adult_model.calculate_color_prior()
+        plot_color_prior(pc)
+        pickle.dump(pc, open("pc.p", "wb"))
     else:
-        print("failed")
-        print("Prior will be calculated during runtime.")
+        pc = pickle.load(open("pc.p", "rb"))
 
-    print("Calculating p(W, C) for languages")
-    models = []  # Array to store fitted model params
-    progress = tqdm(color_data_integer.groupby('language'))
-    for i, (lang_id, elicitations) in enumerate(progress):
-        nd = elicitations.shape[0]  # Number of datapoints for this language
-        nw = elicitations["word"].nunique()  # Number of unique colour terms
-        lang_str = lang_id_lang.loc[lang_id]["language"]  # human-readable name for the language
+    # Run scoring function on various language samples
+    scores = []
+    n_range = np.arange(2, 100, 1)
+    lid = 1  # The language to examine
+    for n in tqdm(n_range):
+        sample = adult_model.sample_languages(n)
+        GaussianLanguageModel.write_samples_file(sample)
 
-        progress.set_postfix({"lang": lang_str, "nd": nd, "nw": nw})
+        child_model = GaussianLanguageModel("sampled_term.txt")
+        child_model.learn_languages(language_ids=[1], progress_bar=False)
+        s = GaussianLanguageModel.score_languages(adult_model, child_model, None)[lid]
+        scores.append(s)
+    scores = np.array(scores)
 
-        # Calculate joint distribution p(W,C|H)
-        pWC_H, _ = learn_language(elicitations)
+    # plt.rcParams.update({"text.usetex": True})
 
-        # Compute prior for language p_l(C) if it doesn't exist
-        plC = None
-        if pC is None:
-            pC_H = pWC_H.sum(axis=0)
-            pW_CH = pWC_H / pC_H
-
-            _, q_wct = ck_blahut_arimoto_ib(pWC_H, 1, pW_CH, "kl-divergence")
-
-            plC = q_wct.sum(axis=0).sum(axis=1)
-            plC /= plC.sum()
-
-        # Sample N points from the joint distribution
-        samples = []
-        flat_joint = pWC_H.flatten()
-        for N in np.arange(N_min, N_max, N_step):
-            sample_idx = np.random.choice(np.arange(nw * C), (N,), True, flat_joint)
-            samples.append(sample_idx)
-
-        model = Model(lang_id, lang_str, nd, nw, pWC_H, plC)
-        models.append((model, samples))
-
-        # FOR TESTING; TODO: REMOVE LATER
-        if i == 0: break
-
-    # Compute colour prior p(C) if it doesn't exist
-    if pC is None:
-        print("Calculating prior - ", end="")
-        pC = np.zeros((C,))
-        L = 0
-        for model, _ in models:
-            if model.lang_id in ["Amuzgo", "Camsa", "Candoshi", "Chayahuita", "Chiquitano", "Eastern Cree", "Carib",
-                                 "Ifugao", "Micmac", "Nahuatl", "Papago", "Slave", "Tacana", "Central Tarahumara",
-                                 "W. Tarahumara"]:
-                continue
-            pC += model.plc
-            L += 1
-        pC /= L
-        pickle.dump(pC, open("pC.p", "wb"))
-        print("done")
-
-        chip_data["row"] = chip_data["row"].apply(lambda x: ord(x) - 65)
-        grid = np.zeros((8, 40))
-        for i, row, col, pos in chip_data.itertuples():
-            if col == 0: continue
-            grid[row - 1, col - 1] = pC[i - 1]
-        plt.imshow(np.flip(grid))
-        plt.show()
-
-    for model, samples in models:
-        inf_loss = []
-        complexity = []
-        pWC_H = model.pwc_h
-        pWC_H_flat = pWC_H.flatten()
-        for sample in samples:
-            # Simplicity prior
-            terms = np.sort(np.unique(sample % model.n_words))  # Colour terms appearing in the sample
-            n_unique = len(terms)
-            pH = expon.pdf(len(sample), scale=pWC_H.shape[0])
-
-            # Compute information loss (log likelihood)
-            ll = np.log(pWC_H_flat[sample]) + np.log(pH)
-            ll[ll == -np.inf] = 0.0
-            inf_loss.append(ll.sum())
-
-            # Compute complexity (mutual information)
-            pC_H = pWC_H[terms, :].sum(axis=0)
-            pC_H /= pC_H.sum()
-            lratio = np.log(pC_H) - np.log(pC)
-            lratio[lratio == -np.inf] = 0
-            complexity.append(np.sum(pC_H * pH * lratio))
-
-        plt.plot(complexity, inf_loss, '.')
-    plt.xlabel("Mutual information")
-    plt.ylabel("Log likelihood")
+    plt.scatter(scores[:, 0], scores[:, 1], c=n_range, cmap="gray_r")
+    for i, coord in enumerate(scores):
+        plt.text(coord[0], coord[1], n_range[i])
+    plt.xlabel("MI(H, C)")
+    plt.ylabel("$D_{KL} [P(W,C) || P(W,C|H)]$")
     plt.show()
